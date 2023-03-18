8c7c5eb8bd62deeefb5593ad573ec826c1c7bc92
Kristuwa <72200499+Kristuwa@users.noreply.github.com>
2023-03-18 20:52:13 +0300

    add register form

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,50 +1,128 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 import {
   ImageBackground,
   StyleSheet,
   Text,
   View,
   TextInput,
-  Button,
   TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
 } from "react-native";
 import image from "./assets/register-bg.jpg";
+
 // import RegistrationScreen from "./screens/RegistrationScreen.js";
 
+const initialState = {
+  login: "",
+  email: "",
+  password: "",
+};
+
 export default function App() {
-  const [login, onChangeLogin] = React.useState("");
-  const [password, onChangePassword] = React.useState("");
-  const [email, onChangeEmail] = React.useState("");
+  const [state, setState] = useState(initialState);
+  const { login, email, password } = state;
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Bold": require("./assets/Fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/Fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/Fonts/Roboto-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  } else {
+    SplashScreen.hideAsync();
+  }
+
+  const keyboardHideSubmitForm = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+    console.log(state);
+    setState(initialState);
+  };
+
+  const keyboardHide = () => {
+    setIsShowKeyboard(false);
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={image} style={styles.image}>
-        <View style={styles.form}>
-          <Text style={styles.title}>Регистрация</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeLogin}
-            value={login}
-            placeholder="Логин"
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangeEmail}
-            value={email}
-            placeholder="Адрес электронной почты"
-          />
+        <TouchableWithoutFeedback onPress={keyboardHide}>
+          <View style={styles.form}>
+            <Text style={styles.title}>Регистрация</Text>
+            <KeyboardAvoidingView
+              behavior={Platform.OS == "ios" ? "padding" : "height"}
+            >
+              <View
+                style={{
+                  ...styles.formContainer,
+                  marginBottom: isShowKeyboard ? 32 : 43,
+                }}
+              >
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, login: value }))
+                  }
+                  value={login}
+                  placeholder="Логин"
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                />
+                <TextInput
+                  style={styles.input}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({ ...prevState, email: value }))
+                  }
+                  value={email}
+                  placeholder="Адрес электронной почты"
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                />
 
-          <TextInput
-            style={styles.input}
-            onChangeText={onChangePassword}
-            value={password}
-            placeholder="Пароль"
-            secureTextEntry={true}
-          />
-          <TouchableOpacity style={styles.btn}>
-            <Text style={styles.btnTitle}>Зарегистрироваться</Text>
-          </TouchableOpacity>
-        </View>
+                <TextInput
+                  style={{ ...styles.input, marginBottom: 0 }}
+                  onChangeText={(value) =>
+                    setState((prevState) => ({
+                      ...prevState,
+                      password: value,
+                    }))
+                  }
+                  value={password}
+                  placeholder="Пароль"
+                  secureTextEntry={true}
+                  onFocus={() => {
+                    setIsShowKeyboard(true);
+                  }}
+                />
+              </View>
+            </KeyboardAvoidingView>
+            <TouchableOpacity
+              style={styles.btn}
+              onPress={keyboardHideSubmitForm}
+            >
+              <Text style={styles.btnTitle}>Зарегистрироваться</Text>
+            </TouchableOpacity>
+            <Text style={styles.bottomText}>Уже есть аккаунт? Войти</Text>
+          </View>
+        </TouchableWithoutFeedback>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -63,10 +141,9 @@ const styles = StyleSheet.create({
     resizeMode: "cover",
   },
   title: {
-    fontWeight: 500,
+    fontFamily: "Roboto-Bold",
     fontSize: 30,
-    lineHeight: 1.17,
-    letterSpacing: "0.01em",
+    //  lineHeight: "1.17",
     marginBottom: 33,
     textAlign: "center",
   },
@@ -79,6 +156,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     borderTopRightRadius: 25,
     borderTopLeftRadius: 25,
+  },
+  formContainer: {
+    justifyContent: "flex-end",
   },
   input: {
     borderWidth: 1,
@@ -104,9 +184,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   btnTitle: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    //  lineHeight: 1.19,
+    color: "#ffffff",
+  },
+  bottomText: {
+    fontFamily: "Roboto-Regular",
     fontWeight: 400,
     fontSize: 16,
-    lineHeight: 1.19,
-    color: "#ffffff",
+    //  lineHeight: 1.18,
+    textAlign: "center",
+    color: "#1B4371",
   },
 });
